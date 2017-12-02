40aee67ed537de99aa3e897de57d71b8162ab531
Joshua Scott <joshuacode16@gmail.com>
2017-12-02 17:17:43 -0600

    change sprite to naruto

--- src/sprites/Naruto.js
+++ src/sprites/Naruto.js
@@ -4,6 +4,7 @@ export default class extends Phaser.Sprite {
   constructor ({ game, x, y, asset }) {
     super(game, x, y, asset)
     this.anchor.setTo(0.5)
+    console.log(this.anchor)
   }
 
   update () {
--- src/states/Splash.js
+++ src/states/Splash.js
@@ -13,7 +13,7 @@ export default class extends Phaser.State {
     //
     // load your assets
     //
-    this.load.image('mushroom', 'assets/images/mushroom2.png')
+    this.load.image('naruto', 'assets/images/8_bit_naruto.png')
   }
 
   create () {
