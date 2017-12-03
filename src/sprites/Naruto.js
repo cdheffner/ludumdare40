b6cd56daa1d593fb6e03555627b79f8f0f28eb7f
Joshua Scott <joshuacode16@gmail.com>
2017-12-02 18:01:12 -0600

    movement on keys

--- src/sprites/Naruto.js
+++ src/sprites/Naruto.js
@@ -1,13 +1,33 @@
 import Phaser from 'phaser'
 
+
 export default class extends Phaser.Sprite {
   constructor ({ game, x, y, asset }) {
     super(game, x, y, asset)
-    this.anchor.setTo(1)
-    this.game.physics.arcade.enable(this)
+    this.anchor.setTo(0.5)
+    console.log(this)
   }
-
+  
+  
   update () {
-    this.angle += 10
+    var sprite1 = this.animations.sprite
+    
+    if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT))
+    {
+        sprite1.x -= 4;
+    }
+    else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT))
+    {
+        sprite1.x += 4;
+    }
+
+    if (game.input.keyboard.isDown(Phaser.Keyboard.UP))
+    {
+        sprite1.y -= 4;
+    }
+    else if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN))
+    {
+        sprite1.y += 4;
+    }
   }
 }
